6cd1965fd7431c99af3f5f38af0c55dd5ba3044e
Liam Atkinson <lca23@sussex.ac.uk>
2023-12-13 14:36:32 +0000

    .

---
+++ namecalc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+class namecalc
+{
+
+
+    static void Main()
+    {
+        // Specify the path to your text file
+        string filePath = "./names.txt";
+
+        // Read the entire line from the file
+        string namesLine = File.ReadAllText(filePath);
+
+        // Split the line into an array of strings using commas as separators
+        string[] namesArray = namesLine.Split(',');
+
+        // Now 'namesArray' contains the names as strings
+
+        // You can loop through the array to see the names, for example:
+        foreach (var name in namesArray)
+        {
+            Console.WriteLine(name);
+        }
+    }
+
+}
